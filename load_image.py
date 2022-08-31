9c4535273ae8c235e4b9a140f4128284495decab
= <=>
2022-08-30 23:52:12 -0500

    Adicion preprocesamiento carga de imagen

--- load_image.py
+++ load_image.py
@@ -18,7 +18,19 @@ def read_jpg_file(path):
     img_array = np.asarray(img)
     img2show = Image.fromarray(img_array) 
     img2 = img_array.astype(float) 
-    img2 = (np.maximum(img2,0) / img2.max()) * 255.0
+    img2 = (np.maximum(img2,0) / img2.max()) * 255.
     img2 = np.uint8(img2)
     return img2, img2show 
+
+
+def preprocess(array):
+     array = cv2.resize(array , (512 , 512))
+     array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4,4))
+     array = clahe.apply(array)
+     array = array/255
+     array = np.expand_dims(array,axis=-1)
+     array = np.expand_dims(array,axis=0)
+     return array
+
 
